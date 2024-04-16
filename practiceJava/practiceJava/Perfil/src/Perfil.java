05750e173428311cb33a2d0d4eb8ae53e909b5a8
Gabriel Turman <80403351+Turman005@users.noreply.github.com>
2024-04-16 11:29:17 -0300

    Add files via upload

---
+++ practiceJava/practiceJava/Perfil/src/Perfil.java
@@ -0,0 +1,9 @@
+
+public class Perfil {
+    public static void main(String[] args) {
+        String perfil = "Olá, Gabriel Turman";
+        String cumprimento = "Tudo bem?";
+        System.out.println(perfil);
+        System.out.println(cumprimento);
+    }
+}
---
+++ practiceJava/practiceJava/untitled/src/PrimeiroProjeto.java
@@ -0,0 +1,20 @@
+
+public class PrimeiroProjeto {
+    public static void main(String[] args) {
+        System.out.println("This is Screen Match");
+        System.out.println("Filme: Top gun: Maverick");
+
+        int anoDeLancamento = 2022;
+        System.out.println("Ano de lançamento" + anoDeLancamento);
+        boolean incluidoNoPlano = true;
+        double notaDoFilme = 8.1;
+
+        double media = (9.8 + 6.3 + 8.0) /3;
+        System.out.println(media);
+        String sinopse = "Filme top gun";
+        System.out.println(sinopse);
+
+        int classificacao;
+        classificacao = (int) (media/2);
+    }
+}
